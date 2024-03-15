955c65e938a76e406509ea3dc3c011be2ec9f2f4
nicoladelbianco <nicoladelbianco97@gmail.com>
2024-03-15 18:21:42 +0100

    update Screen with State

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,17 @@ void main() {
   );
 }
 
-class HomeListViewScreen extends StatelessWidget {
+class HomeListViewScreen extends StatefulWidget {
   HomeListViewScreen({super.key});
 
+  @override
+  State<HomeListViewScreen> createState() => _HomeListViewScreenState();
+}
+
+class _HomeListViewScreenState extends State<HomeListViewScreen> {
   final people = PeopleService().getPeople(results: 100).toList();
+
+  int _currentIndex = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -32,18 +39,26 @@ class HomeListViewScreen extends StatelessWidget {
 
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() {
+          _currentIndex = index;
+        }),
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.list), label: "List"),
           BottomNavigationBarItem(
               icon: Icon(Icons.favorite_outlined), label: "Favorite")
         ],
       ),
-      body: ListView.builder(
-        itemCount: people.length,
-        itemBuilder: (context, index) {
-          return _buildListTile(person: people[index]);
-        },
-      ),
+      body: _currentIndex == 0
+          ? ListView.builder(
+              itemCount: people.length,
+              itemBuilder: (context, index) {
+                return _buildListTile(person: people[index]);
+              },
+            )
+          : const Center(
+              child: Text("Hello World!"),
+            ),
     );
   }
 }
