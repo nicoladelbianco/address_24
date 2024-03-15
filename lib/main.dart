f70f99fc7fd414160ead332bad704ca245686dae
nicoladelbianco <nicoladelbianco97@gmail.com>
2024-03-15 16:32:04 +0100

    added MaterialWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,39 @@
+import 'package:address_24/models/person.dart';
+import 'package:address_24/services/people_service.dart';
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
 
 void main() {
   runApp(
-    SafeArea(
-      child: HomeScreen(),
+    MaterialApp(
+      home: SafeArea(
+        child: HomeListViewScreen(),
+      ),
     ),
   );
+}
+
+class HomeListViewScreen extends StatelessWidget {
+  HomeListViewScreen({super.key});
+
+  final people = PeopleService().getPeople().toList();
+
+  @override
+  Widget build(BuildContext context) {
+    ListTile _buildListTile({required Person person}) {
+      return ListTile(
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(person.picture!.thumbnail!),
+        ),
+        title: Text(person.firstName!),
+        subtitle: Text(person.cell!),
+        trailing: const Icon(Icons.favorite_border),
+      );
+    }
+
+    return Scaffold(
+      body: ListView(
+        children: people.map((e) => _buildListTile(person: e)).toList(),
+      ),
+    );
+  }
 }
